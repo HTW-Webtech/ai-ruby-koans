c2fefef109a3e0e9739cfbf6a9aaa05f67bf2c02
Gregory Igelmund <gregory.igelmund@gmail.com>
2016-05-03 10:12:22 +0200

    Simplify koans for a beginner exercise

--- about_hashes.rb
+++ about_hashes.rb
@@ -20,18 +20,6 @@ class AboutHashes < Neo::Koan
     assert_equal __, hash[:doesnt_exist]
   end
 
-  def test_accessing_hashes_with_fetch
-    hash = { :one => "uno" }
-    assert_equal __, hash.fetch(:one)
-    assert_raise(___) do
-      hash.fetch(:doesnt_exist)
-    end
-
-    # THINK ABOUT IT:
-    #
-    # Why might you want to use #fetch instead of #[] when accessing hash keys?
-  end
-
   def test_changing_hashes
     hash = { :one => "uno", :two => "dos" }
     hash[:one] = "eins"
@@ -66,51 +54,4 @@ class AboutHashes < Neo::Koan
     assert_equal __, hash.values.class
   end
 
-  def test_combining_hashes
-    hash = { "jim" => 53, "amy" => 20, "dan" => 23 }
-    new_hash = hash.merge({ "jim" => 54, "jenny" => 26 })
-
-    assert_equal __, hash != new_hash
-
-    expected = { "jim" => __, "amy" => 20, "dan" => 23, "jenny" => __ }
-    assert_equal __, expected == new_hash
-  end
-
-  def test_default_value
-    hash1 = Hash.new
-    hash1[:one] = 1
-
-    assert_equal __, hash1[:one]
-    assert_equal __, hash1[:two]
-
-    hash2 = Hash.new("dos")
-    hash2[:one] = 1
-
-    assert_equal __, hash2[:one]
-    assert_equal __, hash2[:two]
-  end
-
-  def test_default_value_is_the_same_object
-    hash = Hash.new([])
-
-    hash[:one] << "uno"
-    hash[:two] << "dos"
-
-    assert_equal __, hash[:one]
-    assert_equal __, hash[:two]
-    assert_equal __, hash[:three]
-
-    assert_equal __, hash[:one].object_id == hash[:two].object_id
-  end
-
-  def test_default_value_with_block
-    hash = Hash.new {|hash, key| hash[key] = [] }
-
-    hash[:one] << "uno"
-    hash[:two] << "dos"
-
-    assert_equal __, hash[:one]
-    assert_equal __, hash[:two]
-    assert_equal __, hash[:three]
-  end
 end
--- about_symbols.rb
+++ about_symbols.rb
@@ -33,40 +33,6 @@ class AboutSymbols < Neo::Koan
   # Why do we convert the list of symbols to strings and then compare
   # against the string value rather than against symbols?
 
-  in_ruby_version("mri") do
-    RubyConstant = "What is the sound of one hand clapping?"
-    def test_constants_become_symbols
-      all_symbols_as_strings = Symbol.all_symbols.map { |x| x.to_s }
-
-      assert_equal __, all_symbols_as_strings.include?(__)
-    end
-  end
-
-  def test_symbols_can_be_made_from_strings
-    string = "catsAndDogs"
-    assert_equal __, string.to_sym
-  end
-
-  def test_symbols_with_spaces_can_be_built
-    symbol = :"cats and dogs"
-
-    assert_equal __.to_sym, symbol
-  end
-
-  def test_symbols_with_interpolation_can_be_built
-    value = "and"
-    symbol = :"cats #{value} dogs"
-
-    assert_equal __.to_sym, symbol
-  end
-
-  def test_to_s_is_called_on_interpolated_symbols
-    symbol = :cats
-    string = "It is raining #{symbol} and dogs."
-
-    assert_equal __, string
-  end
-
   def test_symbols_are_not_strings
     symbol = :ruby
     assert_equal __, symbol.is_a?(String)
@@ -79,22 +45,7 @@ class AboutSymbols < Neo::Koan
     assert_equal __, symbol.respond_to?(:reverse)
   end
 
-  # It's important to realize that symbols are not "immutable
-  # strings", though they are immutable. None of the
-  # interesting string operations are available on symbols.
-
-  def test_symbols_cannot_be_concatenated
-    # Exceptions will be pondered further down the path
-    assert_raise(___) do
-      :cats + :dogs
-    end
-  end
-
   def test_symbols_can_be_dynamically_created
     assert_equal __, ("cats" + "dogs").to_sym
   end
-
-  # THINK ABOUT IT:
-  #
-  # Why is it not a good idea to dynamically create a lot of symbols?
 end
